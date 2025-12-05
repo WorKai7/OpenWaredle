51bb49b5f2bb303b6d4339386d0098864883494a
Yanis <yanis.arabah@gmail.com>
2025-12-05 05:02:07 +0100

    fix: fix dumb conflict management

--- openwardle/src/App.vue
+++ openwardle/src/App.vue
@@ -2,25 +2,15 @@
     <HeaderMenuComponent/>
     <CoreComponent/>
     <Chatbot/>
-=======
     <RouterView/>
->>>>>>> 8d08997b4e57cb849c23d484134ba2b4b186f846
 </template>
-
-
-
 
 <script lang="ts" setup>
     import HeaderMenuComponent from './components/HeaderMenu.component.vue';
     import CoreComponent from './components/Core.component.vue';
     import Chatbot from './components/Chatbot.vue';
-=======
     
->>>>>>> 8d08997b4e57cb849c23d484134ba2b4b186f846
 </script>
-
-
-
 
 <style scoped>
 
